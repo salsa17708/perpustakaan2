200aef214ec1c30dc50b5d509d20f301f1ffc2cd
salsa17708 <salsabilaputri17708@gmail.com>
2025-04-19 20:26:59 +0700

    membuat authentication routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\AdminLoginController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,3 +11,6 @@ Route::get('/', function () {
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('login', [LoginController::class, 'login']);
 Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+Route::get('admin/login', [AdminLoginController::class, 'showLoginForm'])->name('admin.login');
+Route::post('admin/login', [AdminLoginController::class, 'login']);
+Route::post('admin/logout', [AdminLoginController::class, 'logout'])->name('admin.logout');
