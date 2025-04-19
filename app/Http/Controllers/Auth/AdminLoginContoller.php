95c9f561f6e21e61f5a924409a9db8afc753be5a
salsa17708 <salsabilaputri17708@gmail.com>
2025-04-19 19:56:28 +0700

    membuat admin login controller

---
+++ app/Http/Controllers/Auth/AdminLoginContoller.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllrs\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminLoginController extends Controller
+
+{
+    use AuthenticatesUsers;
+
+    protected $redirectTo = '/admin/dashboard';
+
+    public function __construct()
+    {
+        $this->middleware('guest:admin')->except('logout');
+    }
+
+    public function showLoginForm()
+    {
+        return view('auth.admin-Login');
+    }
+
+    protected function guard()
+    {
+        return Auth::guard('admin');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::guard('admin')->logout();
+        return redirect('/admin/login');
+    }
+}
